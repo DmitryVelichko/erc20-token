e74bdd4b3e856d3c7b8e8903000fd379434821d5
Dmitry Velichko <soleil-2005@mail.ru>
2023-02-04 09:00:09 +0800

    add token basic smart contract functionality

--- contracts/CalamansiToken.sol
+++ contracts/CalamansiToken.sol
@@ -93,3 +93,27 @@ contract CalamansiToken {
         return true;
     }
 
+    function getTokenHolderData(address _address)
+        public
+        view
+        returns (
+            uint256,
+            address,
+            address,
+            uint256,
+            bool
+        )
+    {
+        return (
+            tokenHolderInfos[_address]._tokenId,
+            tokenHolderInfos[_address]._to,
+            tokenHolderInfos[_address]._from,
+            tokenHolderInfos[_address]._totalToken,
+            tokenHolderInfos[_address]._tokenHolder
+        );
+    }
+
+    function getTokenHolder() public view returns(address[] memory) {
+        return holderToken;
+    }
+}
