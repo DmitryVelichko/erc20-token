e567daa8a5380dc55c990b489991351e16692551
Dmitry Velichko <soleil-2005@mail.ru>
2023-02-06 23:59:25 +0800

    add constants

--- context/constants.js
+++ context/constants.js
@@ -1,3 +1,5 @@
 // 0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512
 
-import CalamansiToken from './CalamansiToken.json'
+import CalamansiToken from './CalamansiToken.json';
+
+export const calamansiToken = '0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512'
